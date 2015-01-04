bf41f91a646fd725e765f7bb2b707ee71d8ca17f
stone725 <stone_725@outlook.jp>
2015-01-05 03:28:16 +0900

    2399追加

---
+++ Volume 23/2399.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i, s, n) for(int i = s; i < (int)n; i++)
+#define per(i, n) for(int i = n; i >= 0; i--)
+#define ROF(i, s, n) for(int i = s; i >= (int)n; i--)
+#define FORIT(i, A) for (auto i : A)
+#define PRINT(x) cout << (x) << "\n"
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define MP make_pair
+#define EACH(i, n) for (__typeof((n).begin()) i = (n).begin(); i != (n).end(); ++i)
+#define SZ(a) int((a).size())
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define SORT(c) sort((c).begin(),(c).end())
+#define CLR(a) memset((a), 0 ,sizeof(a))
+#define dump(x) cout << #x << " = " << (x) << "\n";
+#define debug(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << "\n";
+#define sq(n) (n) * (n)
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef long long LL;
+typedef vector<LL> VLL;
+typedef vector<VLL> VVLL;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+typedef priority_queue<int> maxpq;
+typedef priority_queue< int, vector<int>, greater<int> > minpq;
+static const double EPS = 1e-10;
+static const double PI = acos( -1.0 );
+static const int mod = 10007;
+static const int INF = 1 << 25;
+static const LL LL_INF = 1152921504606846976;
+static const int dx[] = { -1, 0, 1, 0, 1, -1, 1, -1 };
+static const int dy[] = { 0, -1, 0, 1, 1, 1, -1, -1 };
+
+VVI _array;
+VI info_member;
+int n, m;
+
+int main() {
+	while ( ~scanf( "%d", &n ) && n ) {
+		_array.resize( n );
+		for ( int i = 0; i < n; i++ ) {
+			scanf( "%d", &m );
+			_array[ i ].resize( m );
+			for ( int j = 0; j < m; j++ ) {
+				scanf( "%d", &_array[ i ][ j ] );
+			}
+			sort( _array[ i ].begin(), _array[ i ].end() );
+		}
+		scanf( "%d", &m );
+		info_member.resize( m );
+		for ( int i = 0; i < m; i++ ) {
+			scanf( "%d", &info_member[ i ] );
+		}
+		sort( info_member.begin(), info_member.end() );
+		int ans = 0;
+		for ( int i = 0; i < n; i++ ) {
+			if ( _array[ i ].size() >= info_member.size() ) {
+				bool is_can = true;
+				for ( int j = 0; j < info_member.size(); j++ ) {
+					if ( !(binary_search( _array[ i ].begin(), _array[ i ].end(), info_member[ j ] )) ) {
+						is_can = false;
+						break;
+					}
+				}
+				if ( is_can ) {
+					if ( ans ) {
+						ans = -1;
+					}
+					else {
+						ans = i + 1;
+					}
+				}
+			}
+		}
+		printf( "%d\n", ans ? ans : -1 );
+	}
+	return 0;
+}
