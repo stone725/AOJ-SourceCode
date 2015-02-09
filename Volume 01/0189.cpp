4e351525e64f84c298c35c0f21ef5a63ebf9d43d
Kota Ishiyama <stone_725@outlook.jp>
2015-02-10 08:04:29 +0900

    0189をダイクストラで解いた

--- Volume 01/0189.cpp
+++ Volume 01/0189.cpp
@@ -14,8 +14,6 @@ using namespace std;
 #define SORT(c) sort((c).begin(),(c).end())
 #define CLR(a) memset((a), 0 ,sizeof(a))
 #define NCLR(a) memset((a), -1 ,sizeof(a))
-#define dump(x) cout << #x << " = " << (x) << "\n";
-#define debug(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << "\n";
 #define sq(n) (n) * (n)
 typedef vector<int> VI;
 typedef vector<VI> VVI;
@@ -28,96 +26,97 @@ typedef unsigned int uint;
 typedef unsigned long long ull;
 typedef priority_queue<int> maxpq;
 typedef priority_queue< int, vector<int>, greater<int> > minpq;
+typedef complex<double> P;
 static const double EPS = 1e-10;
 static const double PI = acos( -1.0 );
 static const int mod = 1000000007;
-static const int INF = 1 << 25;
-static const LL LL_INF = 1152921504606846976;
+static const int INF = 1 << 29;
+static const LL LL_INF = 1ll << 60;
 static const int dx[] = { -1, 0, 1, 0, 1, -1, 1, -1 };
 static const int dy[] = { 0, -1, 0, 1, 1, 1, -1, -1 };
 
+#ifdef WIN32
+#define dump(x) cout << #x << " = " << (x) << "\n"
+#define debug(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << "\n"
+#else
+#define dump(x)
+#define debug(x)
+#endif
 
-int n, s, g, c;
+struct edge {
+	int to;
+	int cost;
+	edge( int t, int c ) {
+		to = t;
+		cost = c;
+	}
+};
+int n;
+int r[ 10 ][ 10 ];
+vector<edge> e[ 10 ];
 
-VVI _map;
-
-void warshall_floyd( VVI &root ) {
-	for ( int i = 0; i < root.size(); i++ ) {
-		for ( int j = 0; j < root.size(); j++ ) {
-			for ( int k = 0; k < root.size(); k++ ) {
-				root[ j ][ k ] = root[ k ][ j ] = min( root[ j ][ k ], root[ j ][ i ] + root[ i ][ k ] );
+void dijkstra( int s ) {
+	fill( r[ s ], r[ s ] + 10, INF );
+	priority_queue< PII, vector<PII>, greater<PII> > que;
+	que.push( PII( s, 0 ) );
+	while ( que.size() ) {
+		int to = que.top().first;
+		int cost = que.top().second;
+		que.pop();
+		if ( r[ s ][ to ] < cost ) {
+			continue;
+		}
+		r[ s ][ to ] = cost;
+		for ( int i = 0; i < e[ to ].size(); i++ ) {
+			if ( r[ s ][ e[ to ][ i ].to ] > cost + e[ to ][ i ].cost ) {
+				r[ s ][ e[ to ][ i ].to ] = r[ e[ to ][ i ].to ][ s ] = cost + e[ to ][ i ].cost;
+				que.push( PII( e[ to ][ i ].to, cost + e[ to ][ i ].cost ) );
 			}
 		}
 	}
 }
-
-int gcd( int n, int m ) {
-	if ( m == 0 ) {
-		return n;
-	}
-	return gcd( m, n % m );
-}
-
-void make_prime( VI &pm, int max ) {
-	vector<bool> is_pm( max + 1, true );
-	is_pm[ 0 ] = is_pm[ 1 ] = false;
-	for ( int i = 2; i * i <= max; i += 2 ) {
-		if ( is_pm[ i ] ) {
-			for ( int j = i * i; j <= max; j += i ) {
-				is_pm[ j ] = false;
-				if ( i != 2 ) {
-					j += i;
-				}
-			}
-			if ( i == 2 ) {
-				--i;
-			}
-		}
-	}
-	for ( int i = 2; i <= max; i += 2 ) {
-		if ( is_pm[ i ] ) {
-			pm.push_back( i );
-			if ( i == 2 ) {
-				--i;
-			}
-		}
-	}
-}
-
 
 int main() {
+#ifdef WIN32
+	printf( "Debug Mode\n" );
+#endif
 	while ( ~scanf( "%d", &n ) && n ) {
-		_map.resize( 100 );
-		for ( int i = 0; i < 100; i++ ) {
-			_map[ i ].resize( 100 );
-			for ( int j = 0; j < 100; j++ ) {
-				if ( i == j ) {
-					_map[ i ][ j ] = 0;
-				}
-				else {
-					_map[ i ][ j ] = INF;
-				}
+		for ( int i = 0; i < 10; i++ ) {
+			e[ i ].clear();
+		}
+		int m = 0;;
+		for ( int i = 0; i < n; i++ ) {
+			int s, g, c;
+			scanf( "%d %d %d", &s, &g, &c );
+			m = max( max( s, g ), m );
+			dump( m );
+			e[ s ].push_back( edge( g, c ) );
+			e[ g ].push_back( edge( s, c ) );
+		}
+		for ( int i = 0; i < 10; i++ ) {
+			if ( e[ i ].size() ) {
+				m = i;
+			}
+			else {
+				break;
 			}
 		}
-		int max_town = 0;
-		for ( int i = 0; i < n; i++ ) {
-			scanf( "%d %d %d", &s, &g, &c );
-			_map[ s ][ g ] = _map[ g ][ s ] = c;
-			max_town = max( max_town, max( g, s ) + 1 );
+		int ans_cost = INF;
+		int ans_t = 0;
+		for ( int i = 0; i <= m; i++ ) {
+			dijkstra( i );
 		}
-		warshall_floyd( _map );
-		int ans_cost = INF, ans_town;
-		for ( int i = 0; i < max_town; i++ ) {
+		for ( int i = 0; i <= m; i++ ) {
 			int cost = 0;
-			for ( int j = 0; j < max_town; j++ ) {
-				cost += _map[ i ][ j ];
+			for ( int j = 0; j <= m; j++ ) {
+				cost += r[ i ][ j ];
 			}
 			if ( ans_cost > cost ) {
 				ans_cost = cost;
-				ans_town = i;
+				ans_t = i;
 			}
 		}
-		printf( "%d %d\n", ans_town, ans_cost );
+		printf( "%d %d\n", ans_t, ans_cost );
 	}
 	return 0;
 }
